99e315aa267fcdcbb6c801aa03a54bdf78f1eace
Elga <elgaridlosinatriya@gmail.com>
2021-04-13 07:00:16 +0700

    install dotenv, memindahkan beberapa variable ke config.env

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = `mongodb://localhost:27017/vascomm_account`;
+const DB = process.env.DATABASE_LOCAL;
 
 mongoose
     .connect(DB, {
